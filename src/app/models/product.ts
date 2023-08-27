65369bdb6ac39dfa00fa712abc675efdf41e0f21
Sebastian Correa <juancorrea1379@gmail.com>
2023-08-27 17:27:52 -0500

    Creación ingreso de patron al crear nuevo producto

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,7 @@
 import General from "./general";
 import InventoryModel from "./inventory";
 import Supplier from "./supplier";
+import PatternModel from "./pattern";
 
 export default class ProductModel extends General {
     name?: string;
@@ -10,5 +11,6 @@ export default class ProductModel extends General {
     category?: string;
     inventory?: InventoryModel;
     amount?: number;
+    pattern?: PatternModel;
 
 }
